f8f8daef0c756c2cfbd6db991ac2b5c5517a1961
Cankat Örüm <52240938+cankat-orum@users.noreply.github.com>
2022-07-27 09:00:25 +0300

    EMPTY the project
    to build the second version of the website

--- src/components/about.js
+++ src/components/about.js
@@ -2,41 +2,5 @@ import React from "react";
 import "../styles/style.css";
 
 export default function about() {
-  const mystyle = {
-    backgroundColor: "DodgerBlue",
-    width: "250px",
-    height: "250px",
-  };
-  return (
-    <>
-      <main className="about-main">
-        <section className="about-section">
-          <figure>
-            <div style={mystyle}></div>
-          </figure>
-          <article>
-            <header>
-              <h1>Hi, my name's Daniel.</h1>
-            </header>
-            <p>
-              I'm a full stack web developer based in Seattle, WA with a
-              background in technical and customer-focused support. Looking for
-              an opportunity to apply creative, analytical, and communicative
-              skills on a development team.
-            </p>
-            <footer>
-              <ul>
-                <li>
-                  <a href="#">My Work</a>
-                </li>
-                <li>
-                  <a href="#">Hire Me</a>
-                </li>
-              </ul>
-            </footer>
-          </article>
-        </section>
-      </main>
-    </>
-  );
+  return null;
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,24 +2,5 @@ import React from "react";
 import "../styles/style.css";
 
 export default function navbar() {
-  return (
-    <header className="nav-div">
-      <nav className="nav-sem">
-        <a class="logo" href="#">
-          Header Logo
-        </a>
-        <ul class="nav-ul">
-          <li>
-            <a href="#">About Me</a>
-          </li>
-          <li>
-            <a href="#">My Work</a>
-          </li>
-          <li>
-            <a href="#">Hire Me</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+  return null;
 }
